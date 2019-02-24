1910ad37d93dbbca096a926acd7ccee59fd8d092
fontoful <40501207+fontoful@users.noreply.github.com>
2019-02-23 21:45:27 -0600

    Responsiveness
    Making the entire web responsive with more media queries.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // fetching the Dom string
 
-document.getElementById("text").style.visibility = "hidden";
+// document.getElementById("text").style.visibility = "hidden";
 
-let myTitle = document.getElementById("title");
+// let myTitle = document.getElementById("title");
 
-myTitle.addEventListener("mouseover", function() {
-  document.getElementById("text").style.visibility = "visible";
-});
+// myTitle.addEventListener("mouseover", function() {
+//   document.getElementById("text").style.visibility = "visible";
+// });
