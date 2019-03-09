bd3d9dca4551f5b9a278f8092cb1f6d79ad322fd
fontoful <40501207+fontoful@users.noreply.github.com>
2019-03-09 16:44:46 -0600

    Adding image gallery
    fade in class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-// fetching the Dom string
+const current = document.querySelector("#current");
+const imgs = document.querySelector(".imgs");
+const img = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
 
-// document.getElementById("text").style.visibility = "hidden";
+// Set first img opacity
+img[0].style.opacity = opacity;
 
-// let myTitle = document.getElementById("title");
+imgs.addEventListener("click", imgClick);
 
-// myTitle.addEventListener("mouseover", function() {
-//   document.getElementById("text").style.visibility = "visible";
-// });
+function imgClick(e) {
+  // Reset the opacity
+  img.forEach(img => (img.style.opacity = 1));
+
+  // Change current image to src of clicked image
+  current.src = e.target.src;
+
+  // Add fade in class
+  current.classList.add("fade-in");
+
+  // Remove fade-in class after .5 seconds
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+
+  // Change the opacity to opacity var
+  e.target.style.opacity = opacity;
+}
